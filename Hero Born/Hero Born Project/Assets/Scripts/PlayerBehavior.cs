a0de478f1a1fae263ab7eb4f85d8dd6120cea059
Sofila151 <bustams2@my.erau.edu>
2025-02-05 14:06:10 -0700

    CH 8 WebGl

--- Hero Born/Hero Born Project/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Hero Born Project/Assets/Scripts/PlayerBehavior.cs
@@ -36,7 +36,7 @@ using UnityEngine;
     {
       _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
     }
-    
+
     Vector3 rotation = Vector3.up * hInput;
 
     Quaternion angleRot = Quaternion.Euler(rotation *
@@ -47,36 +47,26 @@ using UnityEngine;
 
      _rb.MoveRotation(_rb.rotation * angleRot);
 
-     // 2
          if (Input.GetMouseButtonDown(0))
          {
-             // 3
              GameObject newBullet = Instantiate(bullet,
                 this.transform.position + new Vector3(1, 0, 0),    
                    this.transform.rotation) as GameObject;
 
-             // 4
              Rigidbody bulletRB = 
                  newBullet.GetComponent<Rigidbody>();
- 
-             // 5
-             bulletRB.velocity = this.transform.forward * 
-                                            bulletSpeed;
+
+             bulletRB.velocity = this.transform.forward * bulletSpeed;
          }
 }
-  // 6
      private bool IsGrounded()
      {
-         // 7
          Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
              _col.bounds.min.y, _col.bounds.center.z);
                 
-         // 8
          bool grounded = Physics.CheckCapsule(_col.bounds.center,
             capsuleBottom, distanceToGround, groundLayer,
                QueryTriggerInteraction.Ignore);
-                
-         // 9
          return grounded;
      }
  } 
