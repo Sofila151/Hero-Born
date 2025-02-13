f99360ebbd8674048a6841e3c227f7bf084cdda5
Sofila151 <bustams2@my.erau.edu>
2025-02-13 13:27:25 -0700

    ch 9 complete

--- Hero Born/Hero Born Project/Assets/Scripts/GameBehavior.cs
+++ Hero Born/Hero Born Project/Assets/Scripts/GameBehavior.cs
@@ -3,57 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameBehavior : MonoBehaviour 
- {
+public class GameBehavior : MonoBehaviour
+{
     public bool showWinScreen = false;
+    public bool showLossScreen = false;
 
     public string labeltext = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
 
     private int _itemsCollected = 0;
     public int Items
-     {
-         get { return _itemsCollected; }
-         set { 
-             _itemsCollected = value; 
+    {
+        get { return _itemsCollected; }
+        set
+        {
+            _itemsCollected = value;
 
-            if(_itemsCollected >= maxItems)
-
+            if (_itemsCollected >= maxItems)
             {
                 labeltext = "You've found all the items!";
-
                 showWinScreen = true;
-
                 Time.timeScale = 0f;
             }
             else
             {
                 labeltext = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
             }
-         }
-      }
-     private int _playerHP = 3;
-     public int HP
-     {
-        get {return _playerHP; }
-        set {
-            _playerHP = value;
-            Debug.LogFormat ("Lives: {0}", _playerHP);
         }
-     }
-       void OnGUI()
+    }
+
+    private int _playerHP = 3;
+    public int HP
+    {
+        get { return _playerHP; }
+        set
+        {
+            _playerHP = value;
+
+            if (_playerHP <= 0)
+            {
+                labeltext = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labeltext = "Ouch... that’s gotta hurt.";
+            }
+
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
+    }
+
+    void OnGUI()
     {
         GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerHP);
         GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
         GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labeltext);
 
-        if (showWinScreen)
+        if (showWinScreen && GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You Won!"))
         {
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You Won!"))
-            {
-                SceneManager.LoadScene(0);
-                Time.timeScale = 1.0f;
-            }
+            RestartLevel();
+        }
+
+        if (showLossScreen && GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose..."))
+        {
+            RestartLevel();
         }
     }
 }
--- Hero Born/Hero Born Project/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Hero Born Project/Assets/Scripts/PlayerBehavior.cs
@@ -2,71 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
- public class PlayerBehavior : MonoBehaviour 
+public class PlayerBehavior : MonoBehaviour
 {
-  public float moveSpeed = 10f;
-  public float rotateSpeed = 75f;
-  public float jumpVelocity = 5f;
-  public float distanceToGround = 0.1f;
-  public LayerMask groundLayer;
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
 
-  public GameObject bullet;
-  public float bulletSpeed = 100f;
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
 
-  private float vInput;
-  private float hInput;
-  private Rigidbody _rb;
-  private CapsuleCollider _col;
-        
-  void Start()
+    private float vInput;
+    private float hInput;
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+    private GameBehavior _gameManager;
+
+    void Start()
     {
-      _rb = GetComponent<Rigidbody>();
-      _col = GetComponent<CapsuleCollider>();
-     }
- 
-  void Update()
-  {
-    vInput = Input.GetAxis("Vertical") * moveSpeed;
-    hInput = Input.GetAxis("Horizontal") * rotateSpeed;
-  }
- 
-  void FixedUpdate()
-  {
-    if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-    {
-      _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
+        _gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
     }
 
-    Vector3 rotation = Vector3.up * hInput;
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+    }
 
-    Quaternion angleRot = Quaternion.Euler(rotation *
-        Time.fixedDeltaTime);
+    void FixedUpdate()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
 
-    _rb.MovePosition(this.transform.position +
-        this.transform.forward * vInput * Time.fixedDeltaTime);
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-     _rb.MoveRotation(_rb.rotation * angleRot);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
 
-         if (Input.GetMouseButtonDown(0))
-         {
-             GameObject newBullet = Instantiate(bullet,
-                this.transform.position + new Vector3(1, 0, 0),    
-                   this.transform.rotation) as GameObject;
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+        }
+    }
 
-             Rigidbody bulletRB = 
-                 newBullet.GetComponent<Rigidbody>();
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
+    } 
 
-             bulletRB.velocity = this.transform.forward * bulletSpeed;
-         }
+    void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.name == "Enemy")
+        {
+            _gameManager.HP -= 1;
+        }
+    }
 }
-     private bool IsGrounded()
-     {
-         Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
-             _col.bounds.min.y, _col.bounds.center.z);
-                
-         bool grounded = Physics.CheckCapsule(_col.bounds.center,
-            capsuleBottom, distanceToGround, groundLayer,
-               QueryTriggerInteraction.Ignore);
-         return grounded;
-     }
- } 
